667087a3396ba2403a579a9b7cada6e3b46c8a91
Aaron <aaronpiolang@gmail.com>
2019-11-01 22:15:28 +0000

    functions challenge

--- odd.js
+++ odd.js
@@ -1,11 +1,51 @@
 
 
-const isOdd = (num) => {
-    if (num % 2 === 0) {
-        return false
-    } else {
-        return true
+// const isOdd = (num) => {
+//     if (num % 2 === 0) {
+//         return false
+//     } else {
+//         return true
+//     }
+// }
+
+// console.log("4 is odd: " + isOdd(4))
+
+
+// const ageCalculator = (name, yearOfBirth, currentYear) => {
+//     const age = currentYear - yearOfBirth
+//     return `${name} is ${age} years old.`
+// }
+
+// const howManyHundreds = (num) => {
+//     const howMany = Math.floor(num / 100)
+//     return howMany
+// }
+
+// console.log(howManyHundreds(1000), "=?", 10);
+
+let calculateRectangleArea = (length, width) => {
+    if (length < 0 || width < 0) { 
+        return undefined 
     }
+
+    const area = length * width
+    return area
 }
 
-console.log("4 is odd: " + isOdd(4))
+let calculateTriangleArea = (base, height) => {
+    if (base < 0 || height < 0) { 
+        return undefined 
+    }
+    const area = base * height / 2
+    return area
+}
+
+let calculateCircleArea = (radius) => {
+    if (radius < 0) { 
+        return undefined 
+    }
+    const area = Math.PI * Math.pow(radius, 2)
+    return area
+}
+
+
