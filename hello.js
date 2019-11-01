71e0d3fe876eb00b1577ec5889c42692b30bd6ee
Aaron <aaronpiolang@gmail.com>
2019-11-01 21:33:44 +0000

    more function fun

--- hello.js
+++ hello.js
@@ -1,5 +1,19 @@
-const sayHello  = function (name) {
-    console.log("Hello, " + name);
-  }
+// const sayHello  = function (name) {
+//     console.log("Hello, " + name);
+//   }
 
-sayHello('Aaron')
+// sayHello('Aaron')
+
+
+// const returnSayHello  = function (name) {
+//     return "Hello, " + name;
+//   }
+//   const greeting = returnSayHello('John');
+// //   console.log(greeting);
+
+const isEven  = function (num) {
+    return num % 2 === 0;
+  }
+  
+console.log(isEven(10));
+ 
